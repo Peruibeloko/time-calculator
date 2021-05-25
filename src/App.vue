af51814de9c26a2545c68d90a0a7902c51409ff7
peruibeloko <cmendonca32@gmail.com>
2021-05-25 01:22:32 -0300

    setup

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <h1>Time Calculator</h1>
+    <small>(Just start typing)</small>
+  </header>
+  <entry-list />
+</template>
+
+<script>
+import EntryList from './components/EntryList';
+
+export default {
+  name: 'App',
+  components: {
+    EntryList
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=KoHo&display=swap');
+
+:root {
+  --dark: hsl(0, 0%, 10%);
+  --light: hsl(41, 100%, 89%);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  background-color: var(--dark);
+  color: var(--light);
+  height: 100vh;
+  font-family: 'KoHo', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+header {
+  text-align: center;
+  padding: 5rem 0;
+}
+
+header h1 {
+  font-size: 3rem;
+}
+
+header small {
+  font-size: 1rem;
+}
+</style>
---
+++ src/components/EntryList.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <entry v-for="entry in entryList" :key="entry" :entry="entry" :onConfirm="onConfirm" />
+  </ul>
+</template>
+
+<script>
+import Entry from './Entry.vue';
+
+export default {
+  name: 'EntryList',
+  components: { Entry },
+  data() {
+    return {
+      entryList: [
+        {
+          d: '',
+          h: '',
+          m: '',
+          s: ''
+        },
+        {
+          d: '',
+          h: '',
+          m: '',
+          s: ''
+        }
+      ]
+    };
+  },
+  methods: {
+    onConfirm() {}
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
